8d1e0ef4786fec53a45baee26378ba8cdd840449
Dan Winslow <dwinslow123@me.com>
2017-08-23 09:19:27 -0700

    breadthFirstSearch complete

--- linkedListCycle/linkedListCycle.js
+++ linkedListCycle/linkedListCycle.js
@@ -29,3 +29,31 @@
  * Constraint 3: Do not mutate the original nodes in any way
  * Hint: Search for Floyd's Tortoise and Hare algorithm.
  */
+
+ class Node {
+   constructor(node) {
+     this.value = node;
+     this.next = null;
+   }
+ }
+
+ const hasCycle = (head) => {
+   let slow = head;
+   let fast = head;
+   do {
+     if (fast.next === null) return false;
+     fast = fast.next;
+     if (fast.next === null) return false;
+     fast = fast.next;
+     slow = slow.next;
+   } while (slow !== fast);
+   return true;
+ }
+
+const nodeA = new Node('A');
+const nodeB = nodeA.next = new Node('B');
+const nodeC = nodeB.next = new Node('C');
+const nodeD = nodeC.next = new Node('D');
+const nodeE = nodeD.next = new Node('E');
+
+console.log(hasCycle(nodeA));
